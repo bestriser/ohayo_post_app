48d1e9a1ca455c494253c10a68641a4cdcae49af
YamamuraYuya <sales@best-rise.monster>
2021-01-20 10:53:35 +0900

    #16 「Firebase.initializeApp()」は「App(home:FutureBuilder())」に移動した（「読み込み中」「読み込み完了」「エラー」のステータスを簡単に管理できるため） 参考：「FlutterでFirebaseを使うときのFirebase.initializeApp()の呼び方」の「１つ目(FutureBuilderを使う方法)」 https://qiita.com/edasan/items/f32bc58a4afd0ca92432#%EF%BC%91%E3%81%A4%E7%9B%AEfuturebuilder%E3%82%92%E4%BD%BF%E3%81%86%E6%96%B9%E6%B3%95

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ohayo_post_app/home_screen.dart';
@@ -11,7 +12,36 @@ class App extends StatelessWidget {
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(title: 'おはようポスト'),
+      home: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // エラー時
+          if (snapshot.hasError) {
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: AlertDialog(
+                  title: Text(
+                    'サーバーの接続中にエラーが発生しました...\nアプリを再起動して下さい！',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ),
+            );
+          }
+
+          // Firebaseのinitializeの完了後
+          if (snapshot.connectionState == ConnectionState.done) {
+            return HomeScreen(title: 'おはようポスト');
+          }
+
+          // Firebaseのinitializeの完了待ち
+          return Container(
+            color: Colors.white,
+            child: CupertinoActivityIndicator(radius: 25),
+          );
+        },
+      ),
     );
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ohayo_post_app/counter.dart';
@@ -16,24 +15,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool _isInitialized = false;
-  bool _initializedError = false;
   bool _isLoggedIn = false;
-
-  void _initializeFlutterFire() {
-    try {
-      Firebase.initializeApp().whenComplete(() {
-        setState(() => _isInitialized = true);
-        FirebaseAuth.instance.authStateChanges().listen((User user) {
-          if (user != null) {
-            setState(() => _isLoggedIn = true);
-          }
-        });
-      });
-    } catch (e) {
-      setState(() => _initializedError = true);
-    }
-  }
 
   Future<String> _logout() async {
     String logoutError = '';
@@ -46,143 +28,122 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  void initState() {
-    _initializeFlutterFire();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                _isLoggedIn
-                    ? Column(
-                        children: [
-                          Text(
-                            'You have pushed the button this many times:',
-                          ),
-                          Text(
-                            '${context.watch<Counter>().count}',
-                            style: Theme.of(context).textTheme.headline4,
-                          ),
-                          RaisedButton(
-                            child: Text('ログアウト'),
-                            color: Colors.orange,
-                            textColor: Colors.white,
-                            onPressed: () async {
-                              String _logoutError = await _logout();
-                              if (_logoutError == '') {
-                                showDialog<int>(
-                                  context: context,
-                                  barrierDismissible: false,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text('ログアウトしました！'),
-                                      actionsPadding: EdgeInsets.all(16),
-                                      actions: <Widget>[
-                                        RaisedButton(
-                                          child: Text('OK'),
-                                          color: Colors.orange,
-                                          textColor: Colors.white,
-                                          onPressed: () async {
-                                            setState(() => _isLoggedIn = false);
-                                            Navigator.pop(context);
-                                          },
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              } else {
-                                showDialog<int>(
-                                  context: context,
-                                  barrierDismissible: false,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: Text(_logoutError),
-                                      actionsPadding: EdgeInsets.all(16),
-                                      actions: <Widget>[
-                                        RaisedButton(
-                                          child: Text('OK'),
-                                          color: Colors.orange,
-                                          textColor: Colors.white,
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              }
-                            },
-                          ),
-                        ],
-                      )
-                    : Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          RaisedButton(
-                            child: Text('ログイン'),
-                            color: Colors.orange,
-                            textColor: Colors.white,
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => LoginScreen(),
-                                ),
-                              );
-                            },
-                          ),
-                          const SizedBox(width: 16),
-                          RaisedButton(
-                            child: Text('登録'),
-                            color: Colors.orange,
-                            textColor: Colors.white,
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => RegistrationScreen(),
-                                ),
-                              );
-                            },
-                          ),
-                        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _isLoggedIn
+                ? Column(
+                    children: [
+                      Text(
+                        'You have pushed the button this many times:',
                       ),
-                const SizedBox(height: 16),
-                Column(
-                  children: [
-                    Text('_initialized: ${_isInitialized.toString()}'),
-                    Text('_initializedError: ${_initializedError.toString()}'),
-                    Text('_isLoggedIn: ${_isLoggedIn.toString()}'),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: _isLoggedIn
-              ? FloatingActionButton(
-                  onPressed: () => context.read<Counter>().increment(),
-                  tooltip: 'Increment',
-                  child: Icon(Icons.add),
-                )
-              : const SizedBox.shrink(),
+                      Text(
+                        '${context.watch<Counter>().count}',
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      RaisedButton(
+                        child: Text('ログアウト'),
+                        color: Colors.orange,
+                        textColor: Colors.white,
+                        onPressed: () async {
+                          String _logoutError = await _logout();
+                          if (_logoutError == '') {
+                            showDialog<int>(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('ログアウトしました！'),
+                                  actionsPadding: EdgeInsets.all(16),
+                                  actions: <Widget>[
+                                    RaisedButton(
+                                      child: Text('OK'),
+                                      color: Colors.orange,
+                                      textColor: Colors.white,
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            showDialog<int>(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text(_logoutError),
+                                  actionsPadding: EdgeInsets.all(16),
+                                  actions: <Widget>[
+                                    RaisedButton(
+                                      child: Text('OK'),
+                                      color: Colors.orange,
+                                      textColor: Colors.white,
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RaisedButton(
+                        child: Text('ログイン'),
+                        color: Colors.orange,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                      const SizedBox(width: 16),
+                      RaisedButton(
+                        child: Text('登録'),
+                        color: Colors.orange,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegistrationScreen(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+            const SizedBox(height: 16),
+            Text('_isLoggedIn: ${_isLoggedIn.toString()}'),
+          ],
         ),
-        if (!_isInitialized)
-          Center(child: CupertinoActivityIndicator(radius: 50)),
-        if (_initializedError)
-          AlertDialog(title: Text('initializeFlutterFire()でエラーが出ました')),
-      ],
+      ),
+      floatingActionButton: _isLoggedIn
+          ? FloatingActionButton(
+              onPressed: () => context.read<Counter>().increment(),
+              tooltip: 'Increment',
+              child: Icon(Icons.add),
+            )
+          : const SizedBox.shrink(),
     );
   }
 }
