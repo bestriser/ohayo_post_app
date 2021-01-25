937780f34e284a0bb565891070e7e9e00c1cfc58
YamamuraYuya <sales@best-rise.monster>
2021-01-25 14:18:58 +0900

    #20 initializeDateFormatting()を追加した(無くても動いていたのに突然エラーになった)

--- lib/utility/convert.dart
+++ lib/utility/convert.dart
@@ -1,7 +1,9 @@
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
 class Convert {
   String getJapaneseDateFormat(DateTime dateTime) {
+    initializeDateFormatting('ja');
     final yearMonthDayWeekDay = DateFormat.yMMMEd('ja').format(dateTime);
     final hourMinute = DateFormat('hh:mm').format(dateTime);
     return yearMonthDayWeekDay + hourMinute;
