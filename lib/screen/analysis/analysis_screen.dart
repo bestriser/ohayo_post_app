987515543457c235192b2b99d6e7f15e83c16aca
YamamuraYuya <sales@best-rise.monster>
2021-01-31 03:12:55 +0900

    #35 syncfusion_flutter_chartsを使用して、起床時間のグラフを表示してみた 参考：「syncfusion_flutter_charts 18.4.39」の「Readme」の「チャートの開始」の「グラフ要素を追加する」 https://pub.dev/packages/syncfusion_flutter_charts#add-chart-elements

--- lib/screen/analysis/analysis_screen.dart
+++ lib/screen/analysis/analysis_screen.dart
@@ -1,48 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:ohayo_post_app/model/wake_up_time.dart';
+import 'package:ohayo_post_app/notifier/post_notifier.dart';
+import 'package:ohayo_post_app/utility/convert.dart';
 import 'package:ohayo_post_app/widget/posting_floating_action_button.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class AnalysisScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final List<ChartData> chartData = [
-      ChartData('ねこ', 5),
-      ChartData('お父さん', 8),
-      ChartData('お母さん', 4),
-      ChartData('ぼく', 2)
-    ];
+    final postNtf = Provider.of<PostNotifier>(context);
+    final posts = postNtf.posts
+      ..sort((a, b) => b.createdAt.compareTo(a.createdAt));
+    final wakeUpTimes = posts
+        .map((post) => WakeUpTime(Convert().getMonthDayWeekDay(post.createdAt),
+            Convert().getHourMinute(post.createdAt)))
+        .toList();
 
     return Scaffold(
       appBar: AppBar(title: Text('分析')),
       floatingActionButton: PostingFloatingActionButton(),
       body: Center(
-        child: Container(
-          child: SfPyramidChart(
-            title: ChartTitle(text: '家庭内権力図'),
-            palette: <Color>[
-              Colors.orange[300],
-              Colors.orange[200],
-              Colors.orange[100],
-              Colors.orange[50]
-            ],
-            tooltipBehavior: TooltipBehavior(enable: true),
-            legend: Legend(isVisible: true),
-            series: PyramidSeries<ChartData, String>(
-              dataSource: chartData,
-              xValueMapper: (ChartData data, _) => data.x,
-              yValueMapper: (ChartData data, _) => data.y,
-              dataLabelSettings: DataLabelSettings(isVisible: true),
+        child: Transform.translate(
+          offset: Offset(0, -60),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              // Chart title
+              title: ChartTitle(text: '起床時間'),
+              // Enable legend
+              legend: Legend(isVisible: true),
+              // Enable tooltip
+              tooltipBehavior: TooltipBehavior(enable: true),
+              series: <LineSeries<WakeUpTime, String>>[
+                LineSeries<WakeUpTime, String>(
+                    dataSource: wakeUpTimes,
+                    xValueMapper: (WakeUpTime wakeUpTimes, _) =>
+                        wakeUpTimes.label.toString(),
+                    yValueMapper: (WakeUpTime wakeUpTimes, _) =>
+                        wakeUpTimes.time,
+                    // Enable data label
+                    dataLabelSettings: DataLabelSettings(isVisible: true))
+              ],
             ),
           ),
         ),
       ),
     );
   }
-}
-
-class ChartData {
-  ChartData(this.x, this.y, [this.color]);
-  final String x;
-  final double y;
-  final Color color;
 }
