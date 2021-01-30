d327a5eba0a7435828551410a497d4714fb6670e
YamamuraYuya <sales@best-rise.monster>
2021-01-31 01:09:25 +0900

    #35 syncfusion_flutter_chartsを使用して、サンプルグラフを実装してみた 参考：【flutter】数行でできる！Pyramid Chartを実装してみた！【syncfusion_flutter_charts】 https://www.kzyrepository.com/2020/12/flutter%E6%95%B0%E8%A1%8C%E3%81%A7%E3%81%A7%E3%81%8D%E3%82%8Bpyramid-chart%E3%82%92%E5%AE%9F%E8%A3%85%E3%81%97%E3%81%A6%E3%81%BF%E3%81%9Fsyncfusion_flutter_charts/

--- lib/screen/analysis/analysis_screen.dart
+++ lib/screen/analysis/analysis_screen.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:ohayo_post_app/widget/posting_floating_action_button.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class AnalysisScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<ChartData> chartData = [
+      ChartData('ねこ', 5),
+      ChartData('お父さん', 8),
+      ChartData('お母さん', 4),
+      ChartData('ぼく', 2)
+    ];
+
     return Scaffold(
       appBar: AppBar(title: Text('分析')),
       floatingActionButton: PostingFloatingActionButton(),
+      body: Center(
+        child: Container(
+          child: SfPyramidChart(
+            title: ChartTitle(text: '家庭内権力図'),
+            palette: <Color>[
+              Colors.orange[300],
+              Colors.orange[200],
+              Colors.orange[100],
+              Colors.orange[50]
+            ],
+            tooltipBehavior: TooltipBehavior(enable: true),
+            legend: Legend(isVisible: true),
+            series: PyramidSeries<ChartData, String>(
+              dataSource: chartData,
+              xValueMapper: (ChartData data, _) => data.x,
+              yValueMapper: (ChartData data, _) => data.y,
+              dataLabelSettings: DataLabelSettings(isVisible: true),
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y, [this.color]);
+  final String x;
+  final double y;
+  final Color color;
 }
