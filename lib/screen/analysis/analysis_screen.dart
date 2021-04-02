1c4a81a3269a025a7f746f9ab6816fcadf8a2d32
YamamuraYuya <sales@best-rise.monster>
2021-04-02 23:00:38 +0900

    グラフの描画の問題が解決したので、グラフ画面を分析画面直下に戻した

--- lib/screen/analysis/analysis_screen.dart
+++ lib/screen/analysis/analysis_screen.dart
@@ -1,44 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:ohayo_post_app/model/wake_up_time.dart';
+import 'package:ohayo_post_app/notifier/person_notifier.dart';
 import 'package:ohayo_post_app/notifier/post_notifier.dart';
-import 'package:ohayo_post_app/screen/analysis/wake_up_times_screen.dart';
 import 'package:ohayo_post_app/widget/posting_floating_action_button.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class AnalysisScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final postNtf = Provider.of<PostNotifier>(context);
+    final personNtf = Provider.of<PersonNotifier>(context);
 
     return Scaffold(
       appBar: AppBar(title: Text('分析')),
       floatingActionButton: PostingFloatingActionButton(),
-      body: Card(
-        child: TextButton(
-          child: SizedBox(
-            width: double.infinity,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '起床時間',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: Center(
+        child: Transform.translate(
+          offset: Offset(0, -60),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: SfCartesianChart(
+              // X軸の設定
+              primaryXAxis: CategoryAxis(title: AxisTitle()),
+
+              // Y軸の設定
+              primaryYAxis: NumericAxis(minimum: 3),
+
+              // グラフのタイトル
+              title: ChartTitle(text: '${personNtf.person.nickName}さんの睡眠分析'),
+
+              // グラフの説明
+              legend: Legend(isVisible: true),
+
+              // グラフを選択した時のメニュー
+              tooltipBehavior: TooltipBehavior(
+                enable: true,
+                format: 'point.x：point.y時起',
+              ),
+
+              // グラフをトラックした時の縦線
+              trackballBehavior: TrackballBehavior(
+                enable: true,
+                tooltipSettings: InteractiveTooltip(
+                  enable: true,
+                  color: Colors.red,
                 ),
-                Text('平均：${postNtf.averageWakeUpTimes}時起'),
+              ),
+
+              // グラフデータ
+              series: <LineSeries<WakeUpTime, String>>[
+                LineSeries<WakeUpTime, String>(
+                  name: '起床時間',
+                  dataSource: postNtf.wakeUpTimes,
+                  xValueMapper: (WakeUpTime wakeUpTimes, _) => wakeUpTimes.label.toString(),
+                  yValueMapper: (WakeUpTime wakeUpTimes, _) => wakeUpTimes.time,
+                  sortFieldValueMapper: (WakeUpTime wakeUpTimes, _) => wakeUpTimes.reportAt,
+                  sortingOrder: SortingOrder.ascending,
+
+                  // グラフ上の数字
+                  dataLabelSettings: DataLabelSettings(isVisible: false),
+                ),
+                LineSeries<WakeUpTime, String>(
+                  name: '平均起床時間',
+                  dataSource: postNtf.wakeUpTimes
+                      .map((wakeUpTime) => WakeUpTime(
+                            wakeUpTime.label,
+                            postNtf.averageWakeUpTimes,
+                            wakeUpTime.reportAt,
+                          ))
+                      .toList(),
+                  xValueMapper: (WakeUpTime wakeUpTimes, _) => wakeUpTimes.label.toString(),
+                  yValueMapper: (WakeUpTime wakeUpTimes, _) => wakeUpTimes.time,
+                  sortFieldValueMapper: (WakeUpTime wakeUpTimes, _) => wakeUpTimes.reportAt,
+                  sortingOrder: SortingOrder.ascending,
+
+                  // グラフ上の数字
+                  dataLabelSettings: DataLabelSettings(isVisible: false),
+                ),
               ],
             ),
           ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => WakeUpTimesScreen(),
-              ),
-            );
-          },
         ),
       ),
     );
